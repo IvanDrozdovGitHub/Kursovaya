e6fcc16029d91c889793ab526aa6f56f62d58656
Drozdov_Ivan_BMSTU <ivandrozdov2004@mail.ru>
2024-10-07 20:12:40 +0300

    Server ready

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
-let a =6
-console.log(a)
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен: http://localhost:${PORT}`);
+});
