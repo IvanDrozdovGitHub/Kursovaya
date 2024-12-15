81cdd3ecccdd8d4bc93313552fcc1bc7ce002c86
Drozdov_Ivan_BMSTU <ivandrozdov2004@mail.ru>
2024-12-15 19:57:42 +0300

    modeli

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://79.174.82.125:3000",
     methods: ["GET", "POST"],
     credentials: true
 }));
